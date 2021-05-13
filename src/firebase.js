26b68e6e2993e7ac5f0272cdf7e255e1a87c684f
walesoft28 <waleusman28@gmail.com>
2021-05-13 00:05:41 -0700

    initial setup

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+// var firebaseConfig = {
+//   apiKey: "AIzaSyDNXZLoONM3-_j642rrgu_RPhtpVf7Tyjw",
+//   authDomain: "abdulsamad-usman.firebaseapp.com",
+//   projectId: "abdulsamad-usman",
+//   storageBucket: "abdulsamad-usman.appspot.com",
+//   messagingSenderId: "96767099691",
+//   appId: "1:96767099691:web:b5aca18ed27c0639a8fb6e",
+//   measurementId: "G-1Y5J5Q62MB",
+// };
